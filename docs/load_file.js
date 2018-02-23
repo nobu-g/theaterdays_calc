314402a53ea21209c7052b57d543689293130268
nobu-g <ueda-n127@hotmail.co.jp>
2018-02-24 01:50:33 +0900

    test

--- docs/load_file.js
+++ docs/load_file.js
@@ -12,37 +12,38 @@ class LoadTextFile {
         }
     }
 
-    // HTTP通信用
-    function createXMLHttpRequest(cbFunc)
-    {
-    	var XMLhttpObject = null;
-    	try{
-    		XMLhttpObject = new XMLHttpRequest();
-    	}catch(e){
-    		try{
-    			XMLhttpObject = new ActiveXObject("Msxml2.XMLHTTP");
-    		}catch(e){
-    			try{
-    				XMLhttpObject = new ActiveXObject("Microsoft.XMLHTTP");
-    			}catch(e){
-    				return null;
-    			}
-    		}
-    	}
-    	if (XMLhttpObject) XMLhttpObject.onreadystatechange = cbFunc;
-    	return XMLhttpObject;
-    }
+}
 
-    // XMLHttpRequest のステータスが変わるごとに複数回呼ばれる
-    function displayData()
-    {
-    	if ((httpObj.readyState == 4) && (httpObj.status == 200))
-    	{
-    		document.getElementById("text1").innerText = httpObj.responseText;
-            alert("hasLoaded");
-    	}else{
-    		document.getElementById("text1").innerText = "Loading...";
-    	}
+// HTTP通信用
+function createXMLHttpRequest(cbFunc)
+{
+    var XMLhttpObject = null;
+    try{
+        XMLhttpObject = new XMLHttpRequest();
+    }catch(e){
+        try{
+            XMLhttpObject = new ActiveXObject("Msxml2.XMLHTTP");
+        }catch(e){
+            try{
+                XMLhttpObject = new ActiveXObject("Microsoft.XMLHTTP");
+            }catch(e){
+                return null;
+            }
+        }
     }
+    if (XMLhttpObject) XMLhttpObject.onreadystatechange = cbFunc;
+    return XMLhttpObject;
+}
 
+// XMLHttpRequest のステータスが変わるごとに複数回呼ばれる
+function displayData()
+{
+    if ((httpObj.readyState == 4) && (httpObj.status == 200))
+    {
+        document.getElementById("text1").innerText = httpObj.responseText;
+
+    }else{
+        document.getElementById("text1").innerText = "Loading...";
+    }
+    alert("hasLoaded");
 }
