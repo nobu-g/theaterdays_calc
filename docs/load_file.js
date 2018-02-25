c09f5c8ad2aacc4cfc92b00ed83713c37947a3fe
nobu-g <ueda-n127@hotmail.co.jp>
2018-02-25 18:14:07 +0900

    test

--- docs/load_file.js
+++ docs/load_file.js
@@ -1,71 +1,61 @@
 // サーバー上のbmsファイルを読み込む
-class LoadTextFile {
-    constructor(fileName) {
-        alert("in constructor/tyss");
-        this.bmsText = "ini";
-        this.hasLoaded = true;
-        this.xhr = this.createXMLHttpRequest();
-        if (this.xhr)
-        {
-            this.xhr.open("GET", fileName, false);
-            this.xhr.send(null);
-            // this.xhr.abort();
-        }
-    }
 
-    // HTTP通信用
-    createXMLHttpRequest() {
-        var xhr = null;
+let bmsText = "ini";
+
+function loadTextFile(fileName) {
+    alert("in loadTextFile");
+    let xhr = createXMLHttpRequest();
+    if (xhr)
+    {
+        xhr.open("GET", fileName, false);
+        xhr.send(null);
+        // xhr.abort();
+    }
+}
+
+// HTTP通信用
+function createXMLHttpRequest() {
+    var xhr = null;
+    try{
+        xhr = new XMLHttpRequest();
+    }catch(e){
         try{
-            xhr = new XMLHttpRequest();
+            xhr = new ActiveXObject("Msxml2.XMLHTTP");
         }catch(e){
             try{
-                xhr = new ActiveXObject("Msxml2.XMLHTTP");
+                xhr = new ActiveXObject("Microsoft.XMLHTTP");
             }catch(e){
-                try{
-                    xhr = new ActiveXObject("Microsoft.XMLHTTP");
-                }catch(e){
-                    return null;
-                }
+                return null;
             }
         }
-        if (xhr) {
-            xhr.onreadystatechange = function() {
-                switch ( xhr.readyState ) {
-                case 0:
-                    // 未初期化状態.
-                    document.getElementById("text1").innerText = 'uninitialized!';
-                    break;
-                case 1: // データ送信中.
-                    document.getElementById("text1").innerText = 'loading...';
-                    break;
-                case 2: // 応答待ち.
-                    document.getElementById("text1").innerText = 'loaded.';
-                    break;
-                case 3: // データ受信中.
-                    document.getElementById("text1").innerText = 'interactive... ' + xhr.responseText.length + ' bytes.';
-                    break;
-                case 4: // データ受信完了.
-                    if( xhr.status == 200 || xhr.status == 304 ) {
-                        this._bmsText = xhr.responseText; // responseXML もあり
-                        this._hasLoaded = true;
-                        document.getElementById("text1").innerText = 'COMPLETE!' + this.bmsText + '###' + xhr.responseText;
-                    } else {
-                        document.getElementById("text1").innerText = 'Failed. HttpStatus: ' + xhr.statusText;
-                    }
-                    break;
+    }
+    if (xhr) {
+        xhr.onreadystatechange = function() {
+            switch ( xhr.readyState ) {
+            case 0:
+                // 未初期化状態.
+                document.getElementById("text1").innerText = 'uninitialized!';
+                break;
+            case 1: // データ送信中.
+                document.getElementById("text1").innerText = 'loading...';
+                break;
+            case 2: // 応答待ち.
+                document.getElementById("text1").innerText = 'loaded.';
+                break;
+            case 3: // データ受信中.
+                document.getElementById("text1").innerText = 'interactive... ' + xhr.responseText.length + ' bytes.';
+                break;
+            case 4: // データ受信完了.
+                if( xhr.status == 200 || xhr.status == 304 ) {
+                    bmsText = xhr.responseText; // responseXML もあり
+                    document.getElementById("text1").innerText = 'COMPLETE!';
+                } else {
+                    document.getElementById("text1").innerText = 'Failed. HttpStatus: ' + xhr.statusText;
                 }
-            };
-        }
-
-        return xhr;
+                break;
+            }
+        };
     }
 
-
-    get() {
-        if (this._hasLoaded == true)
-            return this._bmsText;
-        else
-            return "not loaded";
-    }
+    return xhr;
 }
